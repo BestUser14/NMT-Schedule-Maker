d5f15a894c919a99db396e807fde50af57bea0c9
BestUser14 <98570920+BestUser14@users.noreply.github.com>
2025-04-08 15:36:08 -0600

    fixed optimise.py

--- optimise.py
+++ optimise.py
@@ -124,8 +124,8 @@ def show_cal(class_list,semester):
 			new_start=(int(start[0:2])*100)+(int(start[2:4])*1.6666666666)
 			new_end=(int(end[0:2])*100)+(int(end[2:4])*1.6666666666)
 			ax.add_patch(Rectangle((day_to_number[day]+2,new_start),1,new_end-new_start))
-	plt.show()
 	plt.savefig("schedules/calendar.png")
+	plt.show()
 	
 if __name__ == "__main__":
 	#semester = classes.get_semester()
